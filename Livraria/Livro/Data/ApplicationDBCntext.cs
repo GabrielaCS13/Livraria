db0d5da19b6e9b987e5caea904c01f5ea38a9c54
GabrielaCS13 <gabriela.silva.00243@gmail.com>
2023-08-16 16:13:29 -0300

    Adicionando projeto

---
+++ Livraria/Livro/Data/ApplicationDBCntext.cs
@@ -0,0 +1,16 @@
+﻿using Livro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Livro.Data
+{
+    public class ApplicationDBCntext: DbContext
+    {
+        public ApplicationDBCntext(DbContextOptions<ApplicationDBCntext> options) : base(options) 
+        {
+        }
+
+        // aqui são criadas as tabelas 
+
+        public DbSet<EmprestimoModel> Emprestimo { get; set; }
+    }
+}
